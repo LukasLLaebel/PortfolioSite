40d766628f5e1b812a1dd14cb762061149770348
LukasLLaebel <84874374+LukasLLaebel@users.noreply.github.com>
2023-03-29 22:59:34 +0200

    CSS added
    First draft of styling has been added

--- index.php
+++ index.php
@@ -4,15 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/nav.css">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Home</title>
 </head>
 <body>
-    <h1>Portfolio</h1>
-    <ul>
-        <li><a href="about.php">About</a></li>
-        <li><a href="projects.php">Projects</a></li>
-    </ul>
+    <header>
+        <div class="navbar">
+            <div class="logo"><a href="/">Portfolio</a></div>
+            <ul class="links">
+                <li><a href="about.php">About</a></li>
+                <li><a href="projects.php">Projects</a></li>
+            </ul>
+            <a href="#" class="action_btn">Get CV</a>
+            <div class="toggle_btn">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </div>
+        <div class="dropdown_menu">
+            <li><a href="about.php">About</a></li>
+            <li><a href="projects.php">Projects</a></li>
+            <a href="#" class="action_btn">get CV</a>
+        </div>
+    </header>
 
+    <main>
+        <section id="hero">
+            <h1>Overskrift</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Quaerat quod reprehenderit porro repudiandae sapiente architecto fugiat nostrum! 
+                Deserunt, quis id?
+            </p>
+        </section>
+    </main>
+
+    <script>
+        const toggleBtn = document.querySelector('.toggle_btn')
+        const toggleBtnIcon = document.querySelector('.toggle_btn i')
+        const dropDownMenu = document.querySelector('.dropdown_menu')
+
+        toggleBtn.onclick = function() {
+            dropDownMenu.classList.toggle('open')
+            const isOpen = dropDownMenu.classList.contains('open')
+            toggleBtnIcon.classList = isOpen 
+                ? 'fa-solid fa-xmark'
+                : 'fa-solid fa-bars'
+            }
+    </script>
 </body>
 
 </html>
--- projects.php
+++ projects.php
@@ -66,69 +66,95 @@ if (count((array)$data) == $inserted_rows) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/nav.css">
+    <link rel="stylesheet" href="./css/projects.css">
+    <link rel="stylesheet" href="./css/projectTables.css">
     <title>Projects</title>
 </head>
-<style>
-    table, th, td {
-        border:1px solid black;
-      	margin:10px;
-    }
-    .right {
-        float: right;
-      	margin-right:20px;
-    }
-    .left {
-        float:left;
-      	margin-left:20px;
-    }
-</style>
 <body>
-    <h1>Projects</h1>
+    <header>
+        <div class="navbar">
+            <div class="logo"><a href="/">Portfolio</a></div>
+            <ul class="links">
+                <li><a href="about.php">About</a></li>
+                <li><a href="projects.php">Projects</a></li>
+            </ul>
+            <a href="#" class="action_btn">Get CV</a>
+            <div class="toggle_btn">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </div>
+        <div class="dropdown_menu">
+            <li><a href="about.php">About</a></li>
+            <li><a href="projects.php">Projects</a></li>
+            <a href="#" class="action_btn">get CV</a>
+        </div>
+    </header>
 
-    <?php 
-    $data_repo="SELECT * FROM `github_data`";
-    $repos=mysqli_query($connect,$data_repo);
-    if ($repos) {
-        while($row=mysqli_fetch_assoc($repos)) {
-            $id = $row['id'];
-            $name = $row['repo_name'];
-            $description = $row['description'];
-            $created_at = $row['created_at'];
-            $updated_at = $row['updated_at'];
-            $stars = $row['stars'];
-            $language = $row['language'];
-            $visibility = $row['visibility'];
-            $url = $row['url'];
-            
-            echo '<table '.($id % 2 == 0 ? 'class="left"' : 'class="right"'). 'style="width:40%">
-            <tr>
-                <th>'.$name .'</th>
-            </tr>
-            <tr>
-                <td>'.$description.'</td>
-            </tr>
-            <tr>
-                <td>'.$created_at.'</td>
-            </tr>
-            <tr>
-                <td>'.$updated_at.'</td>
-            </tr>
-            <tr>
-                <td>'.$stars.'</td>
-            </tr>
-            <tr>
-                <td>'.$language.'</td>
-            </tr>
-            <tr>
-                <td>'.$visibility.'</td>
-            </tr>
-            <tr>
-                <td><a href="'.$url.'">github</a></td>
-            </tr>
-            </table>';
-        }
-    } 
-    ?>
+
+    <main>
+        <h1>Projects</h1>
+
+        <?php 
+        $data_repo="SELECT * FROM `github_data`";
+        $repos=mysqli_query($connect,$data_repo);
+        if ($repos) {
+            while($row=mysqli_fetch_assoc($repos)) {
+                $id = $row['id'];
+                $name = $row['repo_name'];
+                $description = $row['description'];
+                $created_at = $row['created_at'];
+                $updated_at = $row['updated_at'];
+                $stars = $row['stars'];
+                $language = $row['language'];
+                $visibility = $row['visibility'];
+                $url = $row['url'];
+                
+                echo '<table '.($id % 2 == 0 ? 'class="left"' : 'class="right"'). 'style="width:40%">
+                <tr>
+                    <th>'.$name .'</th>
+                </tr>
+                <tr>
+                    <td>'.$description.'</td>
+                </tr>
+                <tr>
+                    <td>Created at:  '.$created_at.'</td>
+                </tr>
+                <tr>
+                    <td>Last updated:  '.$updated_at.'</td>
+                </tr>
+                <tr>
+                    <td>'.$stars.'</td>
+                </tr>
+                <tr>
+                    <td>'.$language.'</td>
+                </tr>
+                <tr>
+                    <td>'.$visibility.'</td>
+                </tr>
+                <tr>
+                    <td><a href="'.$url.'" target="_blank">github</a></td>
+                </tr>
+                </table>';
+            }
+        } 
+        ?>
+    </main>
+
+    <script>
+        const toggleBtn = document.querySelector('.toggle_btn')
+        const toggleBtnIcon = document.querySelector('.toggle_btn i')
+        const dropDownMenu = document.querySelector('.dropdown_menu')
+
+        toggleBtn.onclick = function() {
+            dropDownMenu.classList.toggle('open')
+            const isOpen = dropDownMenu.classList.contains('open')
+            toggleBtnIcon.classList = isOpen 
+                ? 'fa-solid fa-xmark'
+                : 'fa-solid fa-bars'
+            }
+    </script>
 </body>
 </html>
 
